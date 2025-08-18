64cee5f8d12732bcee58c40bc2cdeedad7c2476a
Nikhil Sharma <Nikhil Sharma>
2025-08-18 11:17:31 +0000

    nikhil commit

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.5.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
